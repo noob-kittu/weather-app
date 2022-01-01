98f17a8b84b7ef68c24fe9f18a16c244d501d136
Kittu <70826024+noob-kittu@users.noreply.github.com>
2022-01-01 21:54:02 +0530

    tired now

--- src/App.js
+++ src/App.js
@@ -2,9 +2,40 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello User</h1>
-      <h2>Enter Your City Name to Get Info about Weather</h2>
+    <div className="container mx-auto text-center">
+      <h1 className="text-3xl text-black-900 p-5 font-extrabold tracking-wide font-sans">
+        Hello User
+      </h1>
+      <h2 className="text-black-300 text-lg tracking-wide">
+        Enter Your City Name to Get Info about Weather
+      </h2>
+      <input
+        className="border-2 py-2 px-5 my-5 rounded-3xl w-8/12 hover:ring-2"
+        type="text"
+        placeholder="City Name"
+      />
+      <div className="information">
+        <div className="row flex py-5 ">
+          <div className="column bg-black-100 w-4/12 mx-2 border-2 p-5">
+            <h1> City Name : Udaipur </h1>
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet consectetur adipiscing elit, maecenas
+              ac non bibendum dictumst justo tempus ut, phasellus mattis vel eu
+              metus montes.{" "}
+            </p>
+          </div>
+          <div className="column bg-black-100 w-8/12 mx-2 border-2 p-5">
+            <h1> City Name : Udaipur </h1>
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet consectetur adipiscing elit, maecenas
+              ac non bibendum dictumst justo tempus ut, phasellus mattis vel eu
+              metus montes.{" "}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
